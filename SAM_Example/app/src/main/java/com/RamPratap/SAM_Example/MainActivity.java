505b5b6fb3623575e799bbf8919ef24557d2fd41
ramprasad12323 <33983641+ramprasad12323@users.noreply.github.com>
2019-12-22 14:39:57 +0530

    Update MainActivity.java

--- SAM_Example/app/src/main/java/com/RamPratap/SAM_Example/MainActivity.java
+++ SAM_Example/app/src/main/java/com/RamPratap/SAM_Example/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        con = new uC_Device(this,this);
+        con = new uC_Device(this);
 
         final Switch s = findViewById(R.id.switch_onoff);
         s.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
@@ -51,6 +51,14 @@ public class MainActivity extends AppCompatActivity {
         con.CP13= uC_Device.Output;
         con.CP12= uC_Device.Input;
         con.CP8= uC_Device.Input;
+        con.CP19=uC_Device.Input;
+        con.CP20=uC_Device.Input;
+        con.CP21=uC_Device.Input;
+        con.CP22=uC_Device.Input;
+        con.CP23=uC_Device.Input;
+        con.CP24=uC_Device.Input;
+        con.CP26=uC_Device.Input;
+        con.CP25=uC_Device.Input;
         con.SP13=uC_Device.LOW;
         int i = con.Configure();
         TextView myAwesomeTextView = findViewById(R.id.textView);
@@ -71,7 +79,7 @@ public class MainActivity extends AppCompatActivity {
     public void send0(View view) {
         int j=con.Read_Data();
         TextView myAwesomeTextView = findViewById(R.id.textView);
-        int[] i={con.RP0,con.RP1,con.RP2,con.RP3,con.RP4,con.RP5,con.RP6,con.RP7,con.RP8,con.RP9,con.RP10,con.RP11,con.RP12,con.RP13};
+        int[] i={con.RP0,con.RP1,con.RP2,con.RP3,con.RP4,con.RP5,con.RP6,con.RP7,con.RP8,con.RP9,con.RP10,con.RP11,con.RP12,con.RP13,con.RP14,con.RP15,con.RP16,con.RP17,con.RP18,con.RP19,con.RP20,con.RP21};
         myAwesomeTextView.setText(j+Arrays.toString(i));
     }
 }
